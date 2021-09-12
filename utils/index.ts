29811f918fdb28e71600095c0fde4a82c19dc9e2
gavr2k02 <gavrilin2k02@gmail.com>
2021-09-12 15:06:28 +0300

    update

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,8 @@
 export function toDateString(value: Date): string {
   return `${value.getFullYear()}-${value.getMonth}-${value.getDate}`;
 }
+
+export function dateStringToDate(dateString: string): Date {
+  const [year, month, day] = dateString.split('-');
+  return new Date(Number.parseInt(year), Number.parseInt(month), Number.parseInt(day));
+}
