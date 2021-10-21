41362c389a8c212a3ac9a68981416c446a67d68a
gavr2k02 <gavrilin2k02@gmail.com>
2021-10-21 21:26:08 +0300

    update ipreset

--- interfaces/IPreset.ts
+++ interfaces/IPreset.ts
@@ -1,5 +1,8 @@
 import { IBaseCard } from './IBaseCard';
+import { IFaculty } from './IFaculty';
 
 export interface IPreset extends IBaseCard {
+  facultyId: string;
+  faculty?: IFaculty;
   cardsIds: string[];
 }
